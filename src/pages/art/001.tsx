12aa02b5752146b2035b21857fe145c6e5ce4c69
liuxinqi <liuxinqi@zdns.cn>
2022-11-16 10:57:33 +0800

    feat: tiledline style

--- src/pages/art/001.tsx
+++ src/pages/art/001.tsx
@@ -3,25 +3,33 @@ import { suffix } from '@/utils'
 export default function TiledLine() {
   const ref = useRef<HTMLCanvasElement>(null)
   let ctx = useRef<CanvasRenderingContext2D>(null).current
-  const list = useRef<Array<Array<number>>>([]).current
   const pendingTask = useRef<Array<() => void>>([]).current
   let count = useRef(0).current
 
-  const [size] = useState<number>(400)
-  const [step] = useState<number>(15)
-  const [refreshTime] = useState<number>(1)
+  const [size] = useState<number>(240)
+  const [step] = useState<Array<number>>([20, 15, 10, 15, 20])
+  const [stepIndex, setStepIndex] = useState<number>(0)
 
+  const [refreshTime] = useState<number>(1)
+  const [strokeColor] = useState<string>('rgb(107,114,128)')
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   function init() {
     const current = ref.current!
     ctx = current.getContext('2d')!
-    ctx.canvas.width = size
-    ctx.canvas.height = size
+    const dpr = window.devicePixelRatio
+    ctx.canvas.width = size * dpr
+    ctx.canvas.height = size * dpr
+    ctx.strokeStyle = strokeColor
+    ctx.scale(dpr, dpr)
     start()
   }
 
   function frame() {
-    const fn = suffix(pendingTask).shift()!
-    fn()
+    if (pendingTask.length) {
+      const fn = suffix(pendingTask).shift()!
+      fn()
+    }
   }
 
   function startFrame() {
@@ -42,10 +50,9 @@ export default function TiledLine() {
   function start() {
     clearCanvas()
     pendingTask.length = 0
-    list.length = 0
-    for (let x = 0; x < size; x += step) {
-      for (let y = 0; y < size; y += step)
-        pendingTask.push(() => draw(x, y, step, step))
+    for (let x = 0; x < size; x += step[stepIndex]) {
+      for (let y = 0; y < size; y += step[stepIndex])
+        pendingTask.push(() => draw(x, y, step[stepIndex], step[stepIndex]))
     }
     startFrame()
   }
@@ -53,6 +60,7 @@ export default function TiledLine() {
   function draw(x: number, y: number, width: number, height: number) {
     const c = ctx!
     const leftToRight = Math.random() >= 0.5
+
     c.beginPath()
     if (leftToRight) {
       c.moveTo(x, y)
@@ -62,20 +70,29 @@ export default function TiledLine() {
       c.moveTo(x + width, y)
       c.lineTo(x, y + height)
     }
-
     c.stroke()
   }
 
-  useMount(() => {
+  function clickHandler() {
+    if (stepIndex === step.length - 1)
+      setStepIndex(0)
+    else
+      setStepIndex(stepIndex + 1)
+    start()
+  }
+
+  useEffect(() => {
     init()
-  })
+  }, [init])
 
   return (
     <div
-      flex justify-center items-center
+      flex-center
     >
       <canvas
-        onClick={() => start()}
+        border-album
+        p-10
+        onClick={() => clickHandler()}
         ref={ref}
       />
     </div>
--- uno.config.ts
+++ uno.config.ts
@@ -1,17 +1,11 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  presetWind,
-  transformerAttributifyJsx,
-} from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWind, transformerAttributifyJsx } from 'unocss'
 
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 border-0 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600 !outline-none'],
-    ['flex-center', 'items-center justify-center'],
+    ['flex-center', 'flex items-center justify-center'],
+    ['border-album', 'border-20 border-black border-dark shadow-current shadow-lg shadow-dark:black'],
   ],
 
   presets: [
@@ -23,7 +17,5 @@ export default defineConfig({
     }),
     presetWind(),
   ],
-  transformers: [
-    transformerAttributifyJsx(),
-  ],
+  transformers: [transformerAttributifyJsx()],
 })
