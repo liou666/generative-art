5d996a2738db6f3ec5f76feab94f1399e28fbdb3
liuxinqi <liuxinqi@zdns.cn>
2022-11-21 13:44:44 +0800

    refact: TiledLine

--- src/pages/001.tsx
+++ src/pages/001.tsx
@@ -1,55 +1,17 @@
-import { suffix } from '@/utils'
 import Pager from '@/components/Pager'
 
 export default function TiledLine() {
-  const ref = useRef<HTMLCanvasElement>(null)
-  let ctx = useRef<CanvasRenderingContext2D>(null).current
-  const pendingTask = useRef<Array<() => void>>([]).current
-  let count = useRef(0).current
+  const [ref, ctx] = useInitCanvas()
+  const { pendingTask, startFrame } = useFrame({ isSuffix: true })
 
-  const [size] = useState<number>(240)
-  const [step] = useState<Array<number>>([20, 15, 10, 15, 20])
+  const step = [20, 15, 10, 15, 20]
   const [stepIndex, setStepIndex] = useState<number>(0)
 
-  const [refreshTime] = useState<number>(1)
-  const [strokeColor] = useState<string>('rgb(107,114,128)')
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  function init() {
-    const current = ref.current!
-    ctx = current.getContext('2d')!
-    const dpr = window.devicePixelRatio
-    ctx.canvas.width = size * dpr
-    ctx.canvas.height = size * dpr
-    ctx.strokeStyle = strokeColor
-    ctx.scale(dpr, dpr)
-    start()
-  }
-
-  function frame() {
-    if (pendingTask.length) {
-      const fn = suffix(pendingTask).shift()!
-      fn()
-    }
-  }
-
-  function startFrame() {
-    requestAnimationFrame(() => {
-      count++
-      if (count % refreshTime === 0)
-        frame()
-      if (pendingTask.length)
-        startFrame()
-    })
-  }
-
-  function clearCanvas() {
-    const c = ctx!
-    c.clearRect(0, 0, size, size)
-  }
-
   function start() {
-    clearCanvas()
+    const c = ctx.current!
+    const { width: size } = c.canvas
+    c.clearRect(0, 0, size, size)
+
     pendingTask.length = 0
     for (let x = 0; x < size; x += step[stepIndex]) {
       for (let y = 0; y < size; y += step[stepIndex])
@@ -59,7 +21,7 @@ export default function TiledLine() {
   }
 
   function draw(x: number, y: number, width: number, height: number) {
-    const c = ctx!
+    const c = ctx.current!
     const leftToRight = Math.random() >= 0.5
 
     c.beginPath()
@@ -82,9 +44,9 @@ export default function TiledLine() {
     start()
   }
 
-  useEffect(() => {
-    init()
-  }, [init])
+  useMount(() => {
+    start()
+  })
 
   return (
     <Pager>
